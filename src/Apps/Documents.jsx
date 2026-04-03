5a197d29bbb9e3f78443d23274cf60bd7cb26222
Chibu O <chibuonyenemezu2023@u.northwestern.edu>
2026-04-03 01:08:53 -0500

    update how documents work and add more

--- src/Apps/Documents.jsx
+++ src/Apps/Documents.jsx
@@ -1,37 +1,55 @@
-import React, { Fragment, useState, forwardRef, useImperativeHandle } from 'react';
+import React, { useState, forwardRef, useImperativeHandle, useEffect } from 'react';
 import { CiTextAlignLeft } from "react-icons/ci";
 import { MdKeyboardBackspace } from "react-icons/md";
 import documents from '../Resources/docsData.json';
+import { documentMap } from '../Resources/documentMap.js';
 import './Documents.css';
 
 export const DocumentsApp = forwardRef((props, ref) => {
     const [selectedDocumentIndex, setSelectedDocumentIndex] = useState(0);
     const [showDocument, setShowDocument] = useState(false);
+    const [fileName, setFileName] = useState('');
+    const [documentContent, setDocumentContent] = useState('');
 
     useImperativeHandle(ref, () => {
         return {
             clearWindow() {
                 setSelectedDocumentIndex(0);
                 setShowDocument(false);
+                setDocumentContent('');
+                setFileName('');
             }
         };
     }, []);
 
-    return (
-        <div className='docs-container-outer'>
-            {showDocument ?
-                <DocumentModal setShowDocument={setShowDocument} doc={documents[selectedDocumentIndex]} index={selectedDocumentIndex} /> :
-                <DocumentsHome setSelectedDocumentIndex={setSelectedDocumentIndex} setShowDocument={setShowDocument} />}
-        </div>
-    )
-});
+    useEffect(() => {
+        if (!fileName) {
+            return;
+        }
+        const fullPath = documentMap[fileName];
+        fetch(fullPath).then(response => response.text()).then(text => setDocumentContent(text));
+    }, [fileName]);
 
-const DocumentsHome = ({ setShowDocument, setSelectedDocumentIndex }) => {
     const openDocument = (index) => {
+        setFileName(documents[index]?.file || '');
         setSelectedDocumentIndex(index);
         setShowDocument(true);
     }
 
+    return (
+        <div className='docs-container-outer'>
+            {showDocument ?
+                <DocumentModal 
+                    setShowDocument={setShowDocument} 
+                    doc={documents[selectedDocumentIndex]} 
+                    documentContent={documentContent}
+                    setDocumentContent={setDocumentContent} /> :
+                <DocumentsHome openDocument={openDocument} />}
+        </div>
+    )
+});
+
+const DocumentsHome = ({ openDocument }) => {
     return (
         <div className='docs-container'>
             <div className='docs-header-div'>
@@ -50,21 +68,22 @@ const DocumentsHome = ({ setShowDocument, setSelectedDocumentIndex }) => {
     );
 }
 
-const DocumentModal = ({ doc, index, setShowDocument }) => {
+const DocumentModal = ({ doc, documentContent, setShowDocument, setDocumentContent }) => {
+    const onClose = () => {
+        console.log('Closing document modal');
+        setShowDocument(false);
+        setDocumentContent('');
+    }
+
     return (
         <div className='document-outer-div'>
             <div className='document-header'>
-                <MdKeyboardBackspace id="document-back-icon" onClick={() => setShowDocument(false)} />
+                <MdKeyboardBackspace id="document-back-icon" onClick={onClose} />
                 <h4 className='document-title'>{doc?.documentName}</h4>
             </div>
             <div className='document-inner-div'>
-                <div className='document-doc' id={`page-${index}`}>
-                    {doc && doc.textContent.map((text, index) => (
-                        <Fragment key={index}>
-                            <p>{text}</p>
-                            {index !== doc.textContent.length - 1 && <br />}
-                        </Fragment>
-                    ))}
+                <div className='document-doc'>
+                    <p id="document-text">{documentContent}</p>
                 </div>
             </div>
         </div>
---
+++ src/Resources/documentMap.js
@@ -0,0 +1,25 @@
+import schedule from './docs/schedule.txt';
+import econHw1 from './docs/econ_hw1.txt';
+import econHw2 from './docs/econ_hw2.txt';
+import econHw3 from './docs/econ_hw3.txt';
+import essay from './docs/essay.txt';
+import german from './docs/german.txt';
+import radioClub from './docs/radio_club.txt';
+import resume from './docs/resume.txt';
+import songs from './docs/songs.txt';
+import syllabus from './docs/syllabus.txt';
+import vendors from './docs/vendors.txt';
+
+export const documentMap = {
+  'schedule.txt': schedule,
+  'econ_hw1.txt': econHw1,
+  'econ_hw2.txt': econHw2,
+  'econ_hw3.txt': econHw3,
+  'essay.txt': essay,
+  'german.txt': german,
+  'radio_club.txt': radioClub,
+  'resume.txt': resume,
+  'songs.txt': songs,
+  'syllabus.txt': syllabus,
+  'vendors.txt': vendors,
+};
