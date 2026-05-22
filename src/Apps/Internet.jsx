153c8b69a091aa7a57dededa96cd72751f75ca19
Chibu O <chibuonyenemezu2023@u.northwestern.edu>
2026-05-22 01:36:31 -0500

    ignore TODO and only show iframe when src set

--- src/Apps/Internet.jsx
+++ src/Apps/Internet.jsx
@@ -22,18 +22,9 @@ export const InternetApp = ({ ref, isMaximized }) => {
         };
     }, []);
 
-    const stopVideo = () => {
-        const videoIframe = document.getElementById("internet-video");
-        videoIframe.src = "";
-        videoIframe.src = videoSrc;
-        // video.pause();
-        // video.currentTime = 0;
-    }
-
     return (
         <div className="internet-app">
-            <iframe id="internet-video" src={src} title="Got Ya" frameborder="0"></iframe>
-            {/* <iframe width="1340" height="754" src="https://www.youtube.com/embed/dQw4w9WgXcQ" title="Rick Astley - Never Gonna Give You Up (Official Video) (4K Remaster)" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe> */}
+            {src && <iframe style={{border: "none"}} id="internet-video" src={src} title="Got Ya"></iframe>}
         </div>
     );
 };
